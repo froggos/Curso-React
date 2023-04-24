cfdd1e673d408c8721bcc4734fd00939392a9a19
Sapo <benja.v.o@hotmail.com>
2023-04-24 16:58:48 -0400

    Emisión de evento al componente padre

--- 04-git-expert-app/src/GifExpertApp.jsx
+++ 04-git-expert-app/src/GifExpertApp.jsx
@@ -5,23 +5,31 @@ const GifExpertApp = () => {
 
     const [categorias, setCategorias] = useState(["Elden Ring", "Jedi Knight III: Jedi Academy"]);
 
-    const agregarCategoria = () => {
-        setCategorias([...categorias, "League of Legends"]);
+    const agregarCategoria = (categoria) => {
+        setCategorias([...categorias, categoria]);
     }
-    
+
     return (
         <>
-        <h1>GifExpertApp</h1>
+            <h1>GifExpertApp</h1>
+            {/* 
+                Versión antigua
+                Le paso como propiedad al componente AgregarCategoria el método setCategorias. Este viene
+                con el array de categorías.
+                
+                Versión nueva
+                Creo un evento onNuevoValor al componente AgregarCategoria y a este le paso el método agregarCategoria, quien recibirá
+                el valor que se emita en el componente hijo.
+            */}
+            <AgregarCategoria onNuevoValor={event => { agregarCategoria(event) }} /*setCategorias={setCategorias}*/></AgregarCategoria>
 
-        <AgregarCategoria setCategorias={setCategorias}></AgregarCategoria>
-
-        <ol>
-            {
-                categorias.map(categoria => {
-                    return <li key={categoria}>{categoria}</li>
-                })
-            }
-        </ol>
+            <ol>
+                {
+                    categorias.map(categoria => {
+                        return <li key={categoria}>{categoria}</li>
+                    })
+                }
+            </ol>
         </>
     );
 }
--- 04-git-expert-app/src/components/AgregarCategoria.jsx
+++ 04-git-expert-app/src/components/AgregarCategoria.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from 'react'
 
+// Dentro de los props de AgregarCategoria está el método setCategorias del component GifExpertApp
 const AgregarCategoria = (props) => {
-    
-    const [valorInput, setValorInput] = useState("");
+
+    const [valorInput, setValorInput] = useState('');
 
     const cambioValor = (valor) => {
         setValorInput(valor);
     }
 
+    // Versión antigua
+
+    // Aquí se extrae el método setCategorias, y se ejecuta un método con callback para utilizar las categorías que ya
+    // venían del método y utilizarlas para agregar una nueva categoría y no reemplazar todas las categorías con la nueva
+    // categoría.
+
+    // Versión nueva
+    // Ahora en las props envío un evento onNuevoValor que hace llamado a un evento del componente padre.
+    // El componente del padre recibe el valor de valorInput como event.
     const enviar = () => {
-        props.setCategorias( categorias => [...categorias, valorInput]);
+        const valorLimpio = valorInput.trim();
+        if (valorLimpio.length <= 1) return;
+        // props.setCategorias(categorias => [...categorias, valorInput]);
+        props.onNuevoValor(valorLimpio);
+        setValorInput('');
     }
 
     return (
-        <form onSubmit={(event) => {event.preventDefault(); enviar()}}>
-            <input type='text' placeholder='Buscar gifs' defaultValue={valorInput} onChange={(e) => {cambioValor(e.target.value)}}></input>
+        <form onSubmit={(event) => { event.preventDefault(); enviar() }}>
+            <input type='text' placeholder='Buscar gifs' value={valorInput} onChange={(e) => { cambioValor(e.target.value) }}></input>
         </form>
     )
 }
